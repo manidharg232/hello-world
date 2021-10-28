cf11dd53e269f963aa3b15993fc5ef3c242a2e39
manidharg232 <manidharg9999@gamil.com>
2021-10-28 17:59:26 +0530

    employee salary is added

--- empwage.sh
+++ empwage.sh
@@ -1,10 +1,16 @@
+
 #!/bin/bash -x
 
 isPresent=1;
 randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
 then
-        echo "Employee is Present";
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
 else
-        echo "Employee is Abscent";
+        salary=0;
 fi
+echo $salary
+
+
